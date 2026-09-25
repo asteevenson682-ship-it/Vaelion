ad549396038e33449314a1068703fa5592edf0eb
asteevenson682-ship-it <asteevenson682@gmail.com>
2026-09-25 01:22:18 -0400

    Create Vaelion Android app project

---
+++ mobile/app/src/main/java/com/vaelion/app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.vaelion.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewFeature
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            WebSettingsCompat.setForceDark(webView.settings, WebSettingsCompat.FORCE_DARK_OFF)
+        }
+
+        setContentView(webView)
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
