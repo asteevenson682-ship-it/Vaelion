301de459a87dc040524c23f07d30da560cca0f2d
asteevenson682-ship-it <asteevenson682@gmail.com>
2026-09-25 01:22:12 -0400

    Create Vaelion Android app project

---
+++ mobile/app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.vaelion.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.vaelion.app"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.webkit:webkit:1.14.0")
+}
